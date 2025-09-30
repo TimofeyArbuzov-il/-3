b6065983c3be31144d8c00f7e2aa807199854fc2
TimofeyArbuzov-il <tima25052007at.@gmail.com>
2025-09-30 20:29:49 +0300

    Update task_1.cs

--- task_1.cs
+++ task_1.cs
@@ -10,7 +10,7 @@ class Program
 {
     static void Main()
     {
-        BankAccountType accountType = BankAccountType.Current
+        BankAccountType accountType = BankAccountType.Current;
         Console.WriteLine("Тип банковского счета: " + accountType);
     }
 }
