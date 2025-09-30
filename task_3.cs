ed0eb7cbbb4e693bee9f8d287e487cde03042ffb
TimofeyArbuzov-il <tima25052007at.@gmail.com>
2025-09-30 20:37:17 +0300

    Update and rename task_1.cs to task_3.cs

---
+++ task_3.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace Homework3_1
+{
+    enum University
+    {
+        KGU,
+        KAI,
+        KHTI
+    }
+
+    struct Employee
+    {
+        public string Name;
+        public University Uni;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.Write("ВУЗ: ");
+            switch (Uni)
+            {
+                case University.KGU: Console.WriteLine("КГУ"); break;
+                case University.KAI: Console.WriteLine("КАИ"); break;
+                case University.KHTI: Console.WriteLine("КХТИ"); break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee worker = new Employee { Name = "Иванов Иван", Uni = University.KAI };
+            worker.PrintInfo();
+        }
+    }
+}
