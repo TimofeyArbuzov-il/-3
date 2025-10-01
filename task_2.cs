82874fe1a892a771104d7b6057bb29579e216426
TimofeyArbuzov-il <tima25052007at.@gmail.com>
2025-10-01 13:34:54 +0300

    Update task_2.cs

--- task_2.cs
+++ task_2.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace task_2.cs
+{
+    enum BankAccountType
+    {
+        Current,   
+        Savings    
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            BankAccountType accountType = BankAccountType.Current;
+            string typeName = accountType == BankAccountType.Current ? "Текущий" : "Сберегательный";
+            Console.WriteLine("Тип банковского счета: " + typeName);
+        }
+    }
+}
