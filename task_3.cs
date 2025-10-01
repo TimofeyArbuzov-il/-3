519b68801c4478b88347003f73df46272d3e76ae
TimofeyArbuzov-il <tima25052007at.@gmail.com>
2025-10-01 13:39:05 +0300

    Update task_3.cs

--- task_3.cs
+++ task_3.cs
@@ -1,1 +1,38 @@
+using System;
 
+namespace task_3.cs
+{
+    enum University
+    {
+        KGU,
+        KAI,
+        KHTI
+    }
+
+    struct Employee
+    {
+        public string Name;
+        public University Uni;
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Имя: {Name}");
+            Console.Write("ВУЗ: ");
+            switch (Uni)
+            {
+                case University.KGU: Console.WriteLine("КГУ"); break;
+                case University.KAI: Console.WriteLine("КАИ"); break;
+                case University.KHTI: Console.WriteLine("КХТИ"); break;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Employee worker = new Employee { Name = "Иванов Иван", Uni = University.KAI };
+            worker.PrintInfo();
+        }
+    }
+}
